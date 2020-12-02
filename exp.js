17af358b652b31ab6b2e96320f7510d6945dc576
ConfidentProgrammer <deepp2618@gmail.com>
2020-12-02 02:43:30 -0500

    Added

--- exp.js
+++ exp.js
@@ -3,4 +3,4 @@ let message = document.querySelectorAll('.letter');
 
 const tl = new TimelineMax();
 
-tl.from(message, 1.5, {opacity:0, rotation:80,stagger:0.15, ease:'bounce.out'});
+tl.from(message, 1.5, {opacity:0, rotation:80,stagger:0.15, ease:'elastic.out'});
